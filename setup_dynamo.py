614a31aef067c531c7b9b2dd111a63c63a989bfd
artsemhatskevich <artem.gatskevich@vsgcommerce.com>
2025-01-23 21:29:40 +0100

    refactor

--- setup_dynamo.py
+++ setup_dynamo.py
@@ -1,19 +1,19 @@
 import boto3
 from decimal import Decimal
 
-def put_item():
-    doc_client = boto3.resource(
-        'dynamodb',
-        endpoint_url='http://localhost:8000',
-        region_name='us-west-2',
-        aws_access_key_id='local',
-        aws_secret_access_key='local'
-    )
+doc_client = boto3.resource(
+    'dynamodb',
+    endpoint_url='http://localhost:8000',
+    region_name='us-west-2',
+    aws_access_key_id='local',
+    aws_secret_access_key='local'
+)
 
+def put_item():
     table = doc_client.Table('orders')
     table.put_item(
         Item={
-            'id': '12346',
+            'id': '12347',
             'customerName': 'John Doe',
             'orderDate': '2025-01-23',
             'amount': Decimal(str(250.75))
@@ -22,14 +22,6 @@ def put_item():
     print('Item added successfully.')
 
 def get_item(order_id):
-    doc_client = boto3.resource(
-        'dynamodb',
-        endpoint_url='http://localhost:8000',
-        region_name='us-west-2',
-        aws_access_key_id='dummy',
-        aws_secret_access_key='dummy'
-    )
-
     table = doc_client.Table('orders')
     response = table.get_item(Key={'id': order_id})
     item = response.get('Item')
@@ -37,5 +29,5 @@ def get_item(order_id):
 
 if __name__ == '__main__':
     put_item()
-    get_item('12345')
+    get_item('12347')
     get_item('12346')
